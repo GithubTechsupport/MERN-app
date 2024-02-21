5a9617ddfa0ab7d86a39b9f2d73806ff254deba0
Kwizbee <kwizbee.business@gmail.com>
2024-02-21 22:47:00 +0100

    w

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     name: "session",
-    keys: [process.env.COOKIE_KEY], // should use as secret environment variable
+    keys: [process.env.COOKIE_KEY],
     httpOnly: true
   })
 );
